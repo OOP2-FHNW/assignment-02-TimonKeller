bbc6e6d94866fca7a0b1a6d5d66a548d7460b7f3
TimonKeller <55238597+TimonKeller@users.noreply.github.com>
2020-03-27 13:47:53 +0100

    Assignment 2

--- src/main/java/transactions/TransactionList.java
+++ src/main/java/transactions/TransactionList.java
@@ -4,8 +4,11 @@ import transactions.Trader;
 import transactions.Transaction;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 /**
  * @author Dieter Holz
@@ -22,11 +25,18 @@ public class TransactionList {
     }
 
     public List<Transaction> transactionsInYear(int year) {
-        return null;
+
+        return allTransactions.stream()
+                .filter(used -> used.getYear() == year)
+                .sorted(Comparator.comparingInt(Transaction::getValue))
+                .collect(Collectors.toList());
     }
 
     public List<String> cities() {
-        return null;
+
+        return allTransactions.stream()
+                .map(used -> used.getTrader().getCity()).distinct()
+                .collect(Collectors.toList());
     }
 
     /**
@@ -34,7 +44,16 @@ public class TransactionList {
      * @return all traders from given city sorted by name.
      */
     public List<Trader> traders(String city) {
-        return null;
+
+         List<Trader> traders =
+                 allTransactions.stream()
+                    .map(Transaction::getTrader)
+                    .filter(used -> used.getCity() == city)
+                    .sorted(Comparator.comparing(Trader::getName))
+                    .distinct()
+                    .collect(Collectors.toList());
+
+          return traders;
     }
 
     /**
@@ -43,7 +62,12 @@ public class TransactionList {
      * @return a Map with the year as key and a list of all transaction of this year as value
      */
     public Map<Integer, List<Transaction>> transactionsByYear() {
-        return null;
+
+        return allTransactions.stream()
+ //               .sorted(Comparator.comparing(Transaction::getYear))
+                .collect(Collectors.groupingBy(Transaction::getYear));
+
+
     }
 
     /**
@@ -51,7 +75,9 @@ public class TransactionList {
      * @return true if there are any trader based in given city
      */
     public boolean traderInCity(String city) {
-        return false;
+
+        return allTransactions.stream()
+              .anyMatch(used -> used.getTrader().getCity().equals(city));
     }
 
     /**
@@ -59,34 +85,54 @@ public class TransactionList {
      * @param to   the trader's new location
      */
     public void relocateTraders(String from, String to) {
+
+        allTransactions.stream()
+                .map(Transaction::getTrader)
+                .filter(used -> used.getCity() == from)
+                .forEach(used -> used.setCity(to));
     }
 
     /**
      * @return the highest value in all the transactions
      */
     public int highestValue() {
-        return 0;
+
+        return allTransactions.stream()
+                .map(Transaction::getValue)
+                .max(Integer::compare).get();
+
     }
 
     /**
      * @return the sum of all transaction values
      */
     public int totalValue() {
-        return 0;
+
+        return allTransactions.stream()
+                .map(Transaction::getValue)
+                .collect(Collectors.summingInt(Integer::intValue));
     }
 
     /**
      * @return the transactions.Transaction with the lowest value
      */
     public Transaction getLowestValueTransaction(){
-        return null;
+
+        return allTransactions.stream()
+                .sorted(Comparator.comparing(Transaction::getValue)).findFirst().get();
     }
 
     /**
      * @return a string of all traders’ names sorted alphabetically
      */
     public String traderNames() {
-        return null;
+
+        return allTransactions.stream()
+                .map(Transaction::getTrader)
+                .map(Trader::getName)
+                .sorted()
+                .distinct()
+                .collect(Collectors.joining(""));
     }
 
 }
